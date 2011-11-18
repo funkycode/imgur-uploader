8db11d6a0af7a1e175fa8bbfa2f0c61b2f973913
Misha K <thezogg@gmil.com>
2011-11-19 00:40:57 +0200

    damn me

--- src/upload.c
+++ src/upload.c
@@ -1,20 +1,108 @@
+#include <stdio.h> 
 #include <curl/curl.h>
 #include <string.h>
-//#include <keys.h>
+#include <stdlib.h>
+#include <mxml.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-struct curl_httppost *post=NULL;  
-struct curl_httppost *last=NULL;
 
-void main() {
-curl_global_init();
-easyhandle = curl_easy_init();
-curl_formadd(&post, &last,   CURLFORM_COPYNAME, "image",   CURLFORM_FILECONTENT, "test.png", CURLFORM_END);
-curl_formadd(&post, &last,   CURLFORM_COPYNAME, "key",   CURLFORM_FILECONTENT, "c4f5cf8a8f1782d6388edb1b05cf8efa04ec64af4", CURLFORM_END);
 
-//curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS, data);
-curl_easy_setopt(easyhandle, CURLOPT_URL, "http:////api.imgur.com/2/upload.xml");
-curl_easy_perform(easyhandle);
+struct MemoryStruct {
+  char *memory;
+    size_t size;
+	};
+
+static size_t response_data(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if (mem->memory == NULL) {
+		     /* out of memory! */ 
+     printf("not enough memory (realloc returned NULL)\n");
+     exit(EXIT_FAILURE);
+   }
+					    
+   memcpy(&(mem->memory[mem->size]), contents, realsize);
+   mem->size += realsize;
+   mem->memory[mem->size] = 0;
+  return realsize;
+
+}
+
+
+
+
+
+
+
+int main(int argc, char *argv[]){
+ CURL *handle;
+ CURLcode result;
+ struct curl_httppost *post=NULL;
+ struct curl_httppost *lastptr=NULL;
+ FILE *fd;
+ mxml_node_t *tree;
+ mxml_node_t *node;
+struct MemoryStruct chunk;
+ 
+   chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */ 
+     chunk.size = 0; 
+
+   printf("START \n");
+   handle = curl_easy_init();
+
+
+
+
+
+
+
+   if (handle){
+
+   /* Fill in the file upload field */ 
+     curl_formadd(&post,
+	              &lastptr,
+					CURLFORM_COPYNAME, "key",
+					CURLFORM_COPYCONTENTS,"0426cb779599180503fd02a0e134ff03",
+					CURLFORM_END);
+																  
+     curl_formadd(&post,
+	              &lastptr,
+	                      CURLFORM_COPYNAME, "image",
+	                      CURLFORM_FILE, "test.png",
+	                      CURLFORM_END);
+	  
+
+
+
+
+
+    curl_easy_setopt(handle, CURLOPT_URL,"http://api.imgur.com/2/upload");
+   // curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, response_data);
+ 
+ 
+    curl_easy_setopt(handle, CURLOPT_HTTPPOST, post);
+//    curl_easy_setopt(handle, CURLOPT_WRITEDATA, fd);
+	  
+	
+  
+  result = curl_easy_perform(handle);
+  
+printf("debug2");
+   tree = mxmlLoadFile(NULL, fd,
+                        MXML_TEXT_CALLBACK);
+ printf("debug3");
+   node = mxmlFindElement(tree, tree, "links",
+		                           "href", NULL,
+								                              MXML_DESCEND);
+
+printf("debug4");
+printf("url is %s\n", node->value.text.string);
 
 curl_formfree(post);
+   }   
+ return 0;
 }
 
